c6f59824a85bb6b4d56ee058879a2b5e4e633671
rudrodip <official.rudrodipsarker@gmail.com>
2023-05-02 08:21:42 +0600

    initial commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// Import routes
+const authRoutes = require('./routes/authRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+const notificationRoutes = require('./routes/notificationRoutes');
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(helmet());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/students', studentRoutes);
+app.use('/api/notifications', notificationRoutes);
+
+module.exports = app;
